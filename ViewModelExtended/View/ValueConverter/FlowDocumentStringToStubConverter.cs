6683d3c5cae9a8eeecbea9f7c3c9a3f3ba5eb0e7
taval <the_hive@live.com>
2022-06-07 06:54:37 -0500

    stub converter handles cases w/ bolding so text doesn't get cut off

--- ViewModelExtended/View/ValueConverter/FlowDocumentStringToStubConverter.cs
+++ ViewModelExtended/View/ValueConverter/FlowDocumentStringToStubConverter.cs
@@ -19,8 +19,12 @@ namespace ViewModelExtended
             Block? firstBlock = flowDocument.Blocks.FirstBlock;
             if (firstBlock == null) return string.Empty;
             Paragraph p = (Paragraph)firstBlock;
-            string text = ((Run)p.Inlines.FirstInline).Text;
-            
+            //string text = ((Run)p.Inlines.FirstInline).Text;
+            string text = String.Empty;
+            foreach (Run run in p.Inlines) {
+                text += run.Text;
+			}
+
             if (text.Length >= 16) {
                 return text.Substring(0, 16);
             }
